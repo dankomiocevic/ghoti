bbb89a6f4c5e1dd8c4687ef9aae2270940193a97
Danko Miocevic <dankomiocevic@gmail.com>
2024-12-28 06:52:49 -0800

    Remove String method

--- internal/errors/connection_errors.go
+++ internal/errors/connection_errors.go
@@ -8,18 +8,10 @@ func (e TranscientError) Error() string {
 	return e.Err
 }
 
-func (e TranscientError) String() string {
-	return e.Err
-}
-
 type PermanentError struct {
 	Err string
 }
 
 func (e PermanentError) Error() string {
-	return e.Err
-}
-
-func (e PermanentError) String() string {
 	return e.Err
 }
