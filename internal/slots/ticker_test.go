c6716249053c0ec218b3af52b517c7d6fe25f481
Danko Miocevic <dankomiocevic@gmail.com>
2025-03-02 23:19:21 -0800

    Improve coverage

--- internal/slots/ticker_test.go
+++ internal/slots/ticker_test.go
@@ -116,3 +116,95 @@ func TestTickerMissingConfig(t *testing.T) {
 		t.Fatalf("Slot must return error")
 	}
 }
+
+func TickerInvalidRefreshRate(t *testing.T) {
+	v := viper.New()
+
+	v.Set("kind", "ticker")
+	v.Set("initial_value", 200)
+	v.Set("refresh_rate", 0) // Invalid refresh rate
+
+	_, err := GetSlot(v, nil, "")
+	if err == nil {
+		t.Fatalf("Slot must return error for invalid refresh rate")
+	}
+}
+
+func TickerNegativeInitialValue(t *testing.T) {
+	v := viper.New()
+
+	v.Set("kind", "ticker")
+	v.Set("initial_value", -1) // Invalid initial value
+	v.Set("refresh_rate", 100)
+
+	_, err := GetSlot(v, nil, "")
+	if err == nil {
+		t.Fatalf("Slot must return error for negative initial value")
+	}
+}
+
+func TickerValidConfig(t *testing.T) {
+	v := viper.New()
+
+	v.Set("kind", "ticker")
+	v.Set("initial_value", 200)
+	v.Set("refresh_rate", 100)
+
+	slot, err := GetSlot(v, nil, "")
+	if err != nil {
+		t.Fatalf("Slot must not return error for valid config: %s", err)
+	}
+
+	tickerSlot := slot.(*tickerSlot)
+	if tickerSlot.value != 200 {
+		t.Errorf("Ticker must be started with 200 value: %d", tickerSlot.value)
+	}
+	if tickerSlot.rate != 100 {
+		t.Errorf("Ticker must have refresh rate of 100: %d", tickerSlot.rate)
+	}
+}
+
+func TickerCanReadWhenUsersEmpty(t *testing.T) {
+	v := viper.New()
+
+	v.Set("kind", "ticker")
+	v.Set("initial_value", 200)
+	v.Set("refresh_rate", 100)
+
+	slot, err := GetSlot(v, nil, "")
+	if err != nil {
+		t.Fatalf("Slot must not return error for valid config: %s", err)
+	}
+
+	read_user, _ := auth.GetUser("read", "pass")
+	if !slot.CanRead(&read_user) {
+		t.Fatalf("we should be able to read when users map is empty")
+	}
+}
+
+func TickerCanWriteWhenUsersEmpty(t *testing.T) {
+	v := viper.New()
+
+	v.Set("kind", "ticker")
+	v.Set("initial_value", 200)
+	v.Set("refresh_rate", 100)
+
+	slot, err := GetSlot(v, nil, "")
+	if err != nil {
+		t.Fatalf("Slot must not return error for valid config: %s", err)
+	}
+
+	write_user, _ := auth.GetUser("write", "pass")
+	if !slot.CanWrite(&write_user) {
+		t.Fatalf("we should be able to write when users map is empty")
+	}
+}
+
+func TickerWriteNonIntegerData(t *testing.T) {
+	slot := loadTickerSlot(t)
+
+	_, err := slot.Write("non-integer", nil)
+	if err == nil {
+		t.Fatalf("Error must be returned when storing non-integer value")
+	}
+}
